a3de4f4c7dad57fc710658486716f7b123ad277c
Hamid Rasool <32512858+r-hamid@users.noreply.github.com>
2020-05-27 00:59:03 +0500

    fix all the issues and remove all unnecessary files and routes

--- app.js
+++ app.js
@@ -1,52 +1,22 @@
 // Required Libraries
 const express = require("express");
 const trello = require("./trello/trello");
-const nightmare = require("./webScraping/nightmare");
 
 // Defining variables and setting others to kick start the server
 const app = express();
 
 app.get("/", function(req, res) {
+	
 	let allCards = trello.getCards();
 	allCards.then( (allCards) => {
 		if(allCards) {
-
-			let cardsData = [];
-			for (let index = 0; index < allCards.length; index++) {
-				cardsData.push({
-					URL: allCards[index].desc,
-					scrapingResult: allCards[index].scrappedData
-				});
-			}
-			res.send(cardsData);
+			let promise = trello.saveAllCards(allCards);
+			promise.then( () => {
+				res.send(" Cards Updated Successfully");
+			});
 		}
-		else res.send("Something went wrong");
 	});
-});
 
-app.get("/scrape", async function(req, res) {
-	let scrappedData = [];
-	scrappedData.push(await nightmare.scraping("https://www.sreality.cz/detail/prodej/byt/1+1/chomutov-chomutov-holesicka/1528512092#img=0&fullscreen=false"));
-	res.send(scrappedData);
-});
-
-app.get("/connectToTrello", function(req, res) {
-	let allCards = trello.getCards();
-	allCards.then( (allCards) => {
-		
-		let saveCardPromise = trello.saveDataToCard(allCards);
-
-		saveCardPromise.then( () => {
-			(allCards) ? res.send(allCards) : res.send("Check console for errors");
-		});
-
-	});
-});
-
-app.get("/puppet", async function(req, res) {
-	let scrappedData = [];
-	scrappedData.push(await puppeteer.scraping("https://www.sreality.cz/detail/prodej/byt/1+1/chomutov-chomutov-holesicka/1528512092#img=0&fullscreen=false"));
-	res.send(scrappedData);
 });
 
 //Starting server
--- trello/trello.js
+++ trello/trello.js
@@ -1,11 +1,11 @@
 // Global variables
 const APP_KEY = "29e61fba6f452ee9ff92c1e697c12454";
 const USER_TOKEN = "878a29d179dc1d85d005237011d85d1666e2c7ce997422101d80b73ae5fe16ea";
+let OUTPUT_LIST = "";
 
 // All libraries
 const Trello = require("trello");
 const nightmare = require("../webScraping/nightmare");
-const postingData = require("./postingData");
 
 // Create trello class object
 const trello = new Trello(APP_KEY, USER_TOKEN);
@@ -15,6 +15,7 @@ function getCards() {
 		trello.getListsOnBoard("5ebc74677f398346f79f2361", (error, boardList) => {
 			if(!error) {
 				inputBoard = boardList.filter( board => board.name == "Input" );
+				OUTPUT_LIST = boardList.filter( board => board.name == "Output" );
 				trello.getCardsOnList(inputBoard[0].id, (error, allCards) => {
 					if(!error) {
 						var forEachPromise = new Promise( (resolveForEach, rejectForEach) => {
@@ -39,29 +40,44 @@ function getCards() {
 	return getCardPromise;
 }
 
-function saveDataToCard(allCards) {
-	var forEachPromise = new Promise( (resolveForEach, rejectForEach) => {
-		allCards.forEach( async (card, index) => {
-			let name = card.scrappedData.title;
-			let desc = card.scrappedData.description+"\n"+card.scrappedData.customer.name+"\n"+"Mail: "+card.scrappedData.customer.email+"\n"+"Phone: " + card.scrappedData.customer.phone;
-			if( ( allCards.length - 1) === index ){
-				// trello.updateCard(card.id, ["name", "desc"], [name, desc], (error, response) => {
-				// 	console.log("First Card has been updated");
-				// });
-				resolveForEach();
-				// trello.updateCardName(card.id, name, () => console.log("Name Updated"));
-				// trello.updateCardDescription(card.id, desc, () => console.log("Description Updated"));
+function saveAllCards(allCards) {
+	let promise = new Promise( (resolve, reject) => {
+		allCards.forEach( (card, index) => {
+			if(card.scrappedData) {
+				let returnedPromise = updateCard(card);
+				returnedPromise.then( () => {
+					if( (allCards.length -1) === index ) resolve();
+				});
 			}
-			await trello.updateCard(card.id, ["name", "desc"], [name, desc]);
-			// trello.updateCardName(card.id, name, () => console.log("Name Updated"));
-			// trello.updateCardDescription(card.id, desc, () => console.log("Description Updated"));
+			
+		});
+	});
+	return promise;
+}
+
+function updateCard(card) {
+
+	let name = card.scrappedData.title;
+	let desc = card.scrappedData.description+"\n\n"+card.scrappedData.customer.name+"\n"+"Mail: "+card.scrappedData.customer.email+"\n"+"Phone: " + card.scrappedData.customer.phone;
+	query = "name="+name+"&desc="+desc;
+	let promise = new Promise ( (resolve, reject) => {
+		trello.updateCardName(card.id, name, () => {
+			trello.addAttachmentToCard(card.id, card.desc, () => {
+				trello.updateCardDescription(card.id, desc, () => {
+					trello.addAttachmentToCard(card.id, card.scrappedData.picture, () => {
+						trello.updateCardList(card.id, OUTPUT_LIST[0].id, () => {
+							resolve();
+						});
+					});
+				});
+			});
 		});
 	});
 
-	return forEachPromise;
+	return promise;
 }
 
 module.exports = {
 	getCards: getCards,
-	saveDataToCard: saveDataToCard
+	saveAllCards: saveAllCards
 };
